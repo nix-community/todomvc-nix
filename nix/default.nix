59a5fc5710f8067cd3837f22b6a8498f53578b0e
zimbatm <zimbatm@zimbatm.com>
2017-08-27 21:35:19 +0100

    cleanup

--- nix/default.nix
+++ nix/default.nix
@@ -1,7 +1,5 @@
 /**
  * This is the entry-point for all nix execution in this project.
- *
- * TODO: pin <nixpkgs>
  */
 import ./nixpkgs {
   # Makes the config pure as well. See <nixpkgs>/top-level/impure.nix:
--- nix/frontend/default.nix
+++ nix/frontend/default.nix
@@ -1,8 +1,6 @@
 { lib, mkYarnPackage, linkNodeModulesHook, srcPath ? ../../frontend }:
 mkYarnPackage {
   src = lib.sources.cleanSource srcPath;
-  packageJson = srcPath + "/package.json";
-  yarnLock = srcPath + "/yarn.lock";
 
   buildPhase = ''
     HOME=/tmp/home yarn build
