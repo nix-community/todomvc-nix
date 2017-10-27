b980497c6df40b7274bdc5d511f1e1a657a01793
zimbatm <zimbatm@zimbatm.com>
2017-10-27 23:18:22 +0100

    cleanup

--- nix/lib/fetchTarball-compat.nix
+++ nix/lib/fetchTarball-compat.nix
@@ -1,6 +1,9 @@
 # fetchTarball version that is compatible between all the versions of Nix
 { url, sha256 }@attrs:
-if builtins.lessThan builtins.nixVersion "1.12" then
+let
+  inherit (builtins) lessThan nixVersion fetchTarball;
+in
+if lessThan nixVersion "1.12" then
   fetchTarball { inherit url; }
 else
   fetchTarball attrs
