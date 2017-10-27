07143b886f35ce79c5660031c2729815af076606
zimbatm <zimbatm@zimbatm.com>
2017-10-27 01:05:19 +0100

    add more nixpkgs examples

---
+++ nix/nixpkgs/example-fetchTarball.nix
@@ -0,0 +1,5 @@
+# Example with fetchTarball
+fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/daf53c9a102e695d9ca0c2b48b231e8f2517264b.tar.gz";
+  #sha256 = "1dsszs9ds17n006yv4il39640msslmhdh3ckg4l0vfq8y11ljvss";
+}
