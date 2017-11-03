d54f437aa499e5f404711324f461f1c73dd12b55
Robert Hensing <robert@roberthensing.nl>
2017-11-03 14:47:51 +0100

    prefer composable haskellPackages.extend
    Even though it is not required in this particular situation.

--- nix/backend/default.nix
+++ nix/backend/default.nix
@@ -1,8 +1,6 @@
 { haskell }:
 # Build a new overlay with our own packages
-haskell.packages.ghc802.override {
-  overrides = self: super: {
-    todobackend-common = self.callPackage ./todobackend-common.nix {};
-    todobackend-scotty = self.callPackage ./todobackend-scotty.nix {};
-  };
-}
+haskell.packages.ghc802.extend (self: super: {
+  todobackend-common = self.callPackage ./todobackend-common.nix {};
+  todobackend-scotty = self.callPackage ./todobackend-scotty.nix {};
+})
