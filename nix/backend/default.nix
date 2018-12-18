e3a034e24acb39be952fe905c4b81640137baa32
Robert Hensing <robert@roberthensing.nl>
2018-12-18 11:13:34 +0100

    Use GHC 8.4.4

--- nix/backend/default.nix
+++ nix/backend/default.nix
@@ -1,6 +1,6 @@
 { haskell }:
 # Build a new overlay with our own packages
-haskell.packages.ghc802.extend (self: super: {
+haskell.packages.ghc844.extend (self: super: {
   todobackend-common = self.callPackage ./todobackend-common.nix {};
   todobackend-scotty = self.callPackage ./todobackend-scotty.nix {};
 })
